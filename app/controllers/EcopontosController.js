491f4ea36bca7f9682f395dec6c3c23d326d7378
daniel <dac.eng17@uea.edu.br>
2020-01-24 09:52:31 -0400

    Correção erros

--- app/controllers/EcopontosController.js
+++ app/controllers/EcopontosController.js
@@ -65,16 +65,17 @@ module.exports = {
         console.log("JSON ID: ")
         console.log(id_busca)
         // Encontrar o motorista com esse ID
-        Ecoponto.find(id_busca).then((ecoponto) => {
+        Ecoponto.findOne(id_busca).then((ecoponto) => {
             // Se entrar no banco
-            
+
             // Se não encontrar nenhum id no banco
-            if(ecoponto.body == null) {
+            if(ecoponto == null) {
                 res.send("Nao encontrou")
             }
             // Se encontrar
             else{
                 res.send(ecoponto)
+                console.log(ecoponto)
             }
         // Se não entrar no banco ou ocorrer qualquer erro.
         }).catch((erro) => {
